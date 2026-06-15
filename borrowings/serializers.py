b1a5c7f92ea32760668a0f9507c1d75d8edc65bd
Dmytro <dimkanividimka4@gmail.com>
2026-06-15 20:35:52 +0300

    a:Implement create a serializer

--- borrowings/serializers.py
+++ borrowings/serializers.py
@@ -1,7 +1,32 @@
+from django.core.exceptions import ValidationError
 from rest_framework import serializers
 
 from book.serializers import BookReadSerializer
 from borrowings.models import Borrowing
+from user.models import User
+
+
+class BorrowingCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Borrowing
+        fields = [
+            "borrow_date",
+            "expected_return",
+            "book_id",
+            "user_id",
+        ]
+        read_only_fields = ("id",)
+        user_id = serializers.PrimaryKeyRelatedField(
+            queryset=User.objects.all(), required=True
+        )
+
+
+    def create(self, validated_data):
+        try:
+            borrowing = Borrowing.objects.create(**validated_data)
+            return borrowing
+        except ValidationError as e:
+            raise serializers.ValidationError(e.message_dict)
 
 
 class BorrowingReadSerializer(serializers.ModelSerializer):
