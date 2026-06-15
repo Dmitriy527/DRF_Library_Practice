9e100c3556a6ba0c7522ba3693ece6bc1697924a
Dmytro <dimkanividimka4@gmail.com>
2026-06-15 21:37:03 +0300

    a: Make sure each non-admin can see only their own borrowings

--- borrowings/views.py
+++ borrowings/views.py
@@ -16,6 +16,15 @@ class BorrowingsViewSet(generics.ListCreateAPIView):
             return BorrowingReadSerializer
         return BorrowingCreateSerializer
 
+    def get_queryset(self):
+        queryset = self.queryset
+        current_user_id = self.request.user.id
+
+        if self.request.user.is_staff is False:
+            queryset = queryset.filter(user_id=current_user_id)
+
+        return queryset.distinct()
+
 
 class BorrowingsReadViewSet(generics.RetrieveAPIView):
     queryset = Borrowing.objects.all()
