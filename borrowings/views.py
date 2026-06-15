3df837d85935eda6232a50a6aef6cd550940c5e0
Dmytro <dimkanividimka4@gmail.com>
2026-06-15 20:23:59 +0300

    d: Implement the list & detail endpoints

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
-# Create your views here.
+from borrowings.models import Borrowing
+from borrowings.serializers import (
+    BorrowingReadSerializer,
+)
+
+
+class BorrowingsViewSet(generics.ListCreateAPIView):
+    queryset = Borrowing.objects.all()
+    serializer_class = BorrowingReadSerializer
+    permission_classes = [AllowAny]
+
+
+class BorrowingsReadViewSet(generics.RetrieveAPIView):
+    queryset = Borrowing.objects.all()
+    serializer_class = BorrowingReadSerializer
+    permission_classes = [
+        IsAuthenticated,
+    ]
+
